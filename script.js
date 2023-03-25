5d25c68163959b9df411fec18834d258cf291446
KariminK <103779342+KariminK@users.noreply.github.com>
2023-03-25 11:17:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,48 @@
 const search = document.querySelector("#search");
 const searchBtn = document.querySelector(".searchBtn");
 const userlist = document.querySelector(".userslist");
-let users2 = [];
 let users = []
 searchBtn.addEventListener('click', ()=>{
-    users2 = search.value.split(",");
-    users2.forEach(el=>{
-        users.push(el.trim());
-    })
+    users = search.value.split(", ");
     console.log(users);
     userlist.innerHTML = "";
-    mainfunc();
+    // mainfunc();
     search.value = "";
 })
 
 const createUserElement = (profileImgLink, username, bio, createData, linkToProfile)=>{
-    let profileIMG = document.createElement("img");
+    const profileIMG = document.createElement("img");
     profileIMG.classList.add("userImg");
     profileIMG.src = profileImgLink;
 
-    let userDetailsDiv = document.createElement("div");
+    const userDetailsDiv = document.createElement("div");
     userDetailsDiv.classList.add("userdetails");
-    let usernameP = document.createElement("p");
+    const usernameP = document.createElement("p");
     usernameP.innerText = username;
     usernameP.classList.add("username");
     userDetailsDiv.appendChild(usernameP);
-    let bioP = document.createElement('p');
+    const bioP = document.createElement('p');
     bioP.innerText = bio;
     bioP.classList.add("bio");
     userDetailsDiv.appendChild(bioP);
 
-    let createDateDiv = document.createElement("div");
+    const createDateDiv = document.createElement("div");
     createDateDiv.classList.add("accCreated");
-    let createDateP1 = document.createElement("p");
+    const createDateP1 = document.createElement("p");
     createDateP1.innerText = "Konto utworzone w:";
     createDateDiv.appendChild(createDateP1);
-    let createDateP2 = document.createElement("p");
+    const createDateP2 = document.createElement("p");
     createDateP2.innerText = createData;
     createDateDiv.appendChild(createDateP2);
 
-    let goToProfileLink = document.createElement("a");
+    const goToProfileLink = document.createElement("a");
     goToProfileLink.href = linkToProfile;
     goToProfileLink.classList.add("goToProfile");
-    let goToProfileIcon = document.createElement("img");
+    const goToProfileIcon = document.createElement("img");
     goToProfileIcon.src = "assets/github-mark-white.png"; 
     goToProfileLink.appendChild(goToProfileIcon);
 
-    let liEl = document.createElement("li");
+    const liEl = document.createElement("li");
     liEl.appendChild(profileIMG);
     liEl.appendChild(userDetailsDiv);
     liEl.appendChild(createDateDiv);
